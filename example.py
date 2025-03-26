200892e37212e1b848cc73959ba742fe152379fa
Arnav7714 <poojascorner2020@gmail.com>
2025-03-26 19:11:11 +0530

    hello

---
+++ example.py
@@ -0,0 +1,10 @@
+name= input("may i know your name")
+print(f"nice to meet {name}")
+feeling= input ("how are you feeling today (good/bad) ")
+mood= feeling .lower()
+if mood== "good" :
+    print ("nice")
+elif mood== "bad":
+    print ("that's bad")
+else:
+    print ("Fine")
