c2ad35d7923b6938094afcb6e1b46aa8fa372ca4
Arnav7714 <poojascorner2020@gmail.com>
2025-04-02 16:15:36 +0530

    hello AI/example.py

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
-name= input("may i know your name")
-print(f"nice to meet {name}")
-feeling= input ("how are you feeling today (good/bad) ")
-mood= feeling .lower()
-if mood== "good" :
-    print ("nice")
-elif mood== "bad":
-    print ("that's bad")
+print("hello there! I am AI BOT, what's your name? : ")
+name = input()
+print(f"nice to meet you, {name}.")
+print("How are you doing today? (good/bad) : ")
+mood = input().lower()
+if mood == "good":
+    print("That's good!")
+elif mood == "bad":
+    print("that not nice hope your day get's better")
 else:
-    print ("Fine")asd
-    
+    print("I know sometimes it is hard to put feelings in words.")
